d1f2e531a4b6466a2f3970ab28b6082f33f8a181
Bambang Gunawan <bambanggunawan887@gmail.com>
2021-02-24 23:13:00 +0700

    "Nambah animasi"

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener("DOMContentLoaded", function(event) {
+//     console.log("hey");
+//     console.log(event);
+//   });
+
+// document.addEventListener('readystatechange', event => {
+//     if (event.target.readyState === 'interactive') {
+
+//       console.log(event);
+//     }
+//     else if (event.target.readyState === 'complete') {
+//         const b = document.getElementById("loaders")
+//         b.classList.remove('loader');
+//     }
+//   });
