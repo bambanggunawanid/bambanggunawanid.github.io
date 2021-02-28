1c29b4653f65fcff5d3c53575dad650e09acb911
Bambang Gunawan <bambanggunawan887@gmail.com>
2021-02-28 13:01:46 +0700

    Adding Pages, do Some Animation, Avoid small screen

--- js/script.js
+++ js/script.js
@@ -1,15 +1,70 @@
-// document.addEventListener("DOMContentLoaded", function(event) {
-//     console.log("hey");
-//     console.log(event);
-//   });
+(function ($) {
+  const $WIN = $(window);
+  const cfg = {
+    scrollDuration: 800, // smoothscroll duration
+    mailChimpURL: "", // mailchimp url
+  };
+  const hello = function () {
+    const item = document.getElementById("hello");
+    item.addEventListener("mouseover", function (event) {
+      event.target.style.color = "white";
+      item.innerText = "Hello..";
+    });
+    item.addEventListener("mouseout", function (event) {
+      event.target.style.color = "#2bb2ff";
+      item.innerText = "Hello.";
+    });
+  };
 
-// document.addEventListener('readystatechange', event => {
-//     if (event.target.readyState === 'interactive') {
+  const ssPreloader = function () {
+    $("html").addClass("ss-preload");
 
-//       console.log(event);
-//     }
-//     else if (event.target.readyState === 'complete') {
-//         const b = document.getElementById("loaders")
-//         b.classList.remove('loader');
-//     }
-//   });
+    $WIN.on("load", function () {
+      $("html, body").animate({ scrollTop: 0 }, "normal");
+
+      $("#loader").fadeOut("slow", function () {
+        $("#preloader").delay(400).fadeOut("slow");
+      });
+
+      // for hero content animations
+      $("html").removeClass("ss-preload");
+      $("html").addClass("ss-loaded");
+    });
+  };
+
+  const ssSmoothScroll = function () {
+    $(".smoothscroll").on("click", function (e) {
+      const target = this.hash;
+      const $target = $(target);
+
+      e.preventDefault();
+      e.stopPropagation();
+
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $target.offset().top,
+          },
+          cfg.scrollDuration,
+          "swing"
+        )
+        .promise()
+        .done(function () {
+          window.location.hash = target;
+        });
+    });
+  };
+  /* initialize
+   * ------------------------------------------------------ */
+  (function ssInit() {
+    console.log(window.innerWidth);
+    if (window.innerWidth < 1200) {
+      $(".screenNotSupport").css("visibility", "visible");
+      $("body").css("overflow", "hidden");
+    }
+    ssPreloader();
+    ssSmoothScroll();
+    hello();
+  })();
+})(jQuery);
